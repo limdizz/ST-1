eed87f65e797d11f40fe237ad0159d91d4da52d2
Vladislav Klimenko <vlad.klimenko.mmv@bk.ru>
2026-02-18 21:03:51 +0300

    replace int with uint64_t in the functions & optimize them

--- src/alg.cpp
+++ src/alg.cpp
@@ -3,49 +3,45 @@
 #include <cstdint>
 
 bool checkPrime(uint64_t value) {
-  bool isPrime = true;
-  if (value == 0 || value == 1) {
-    isPrime = false;
+  if (value <= 1) {
+    return false;
   }
 
-  for (int i = 2; i * i <= value; i++) {
+  for (uint64_t i = 2; i * i <= value; i++) {
     if (value % i == 0) {
-      isPrime = false;
-      break;
+      return false;
     }
   }
-  return isPrime;
+  return true;
 }
 
 uint64_t nPrime(uint64_t n) {
-  int pos = 1;
-  int num = 2;
-  if (n == 1)
-    return 2;
+  if (n == 0)
+    return 0;
+  uint64_t pos = 1;
+  uint64_t num = 2;
+
   while (pos < n) {
     num++;
-    if (checkPrime(num) == true)
+    if (checkPrime(num))
       pos++;
   }
   return num;
 }
 
 uint64_t nextPrime(uint64_t value) {
-  value += 1;
-  while (checkPrime(value) != true) {
-    value++;
+  uint64_t candidate = value + 1;
+  while (!checkPrime(candidate)) {
+    candidate++;
   }
-  return value;
+  return candidate;
 }
 
 uint64_t sumPrime(uint64_t hbound) {
-  uint64_t first = 2;
-  uint64_t sumP = 0;
-  if (hbound <= 2)
-    return 0;
-  while (first < hbound) {
-    sumP += first;
-    first = nextPrime(first);
+  uint64_t sum = 0;
+  for (uint64_t i = 2; i < hbound; i++) {
+    if (checkPrime(i))
+      sum += i;
   }
-  return sumP;
+  return sum;
 }
